18959234594771c6a07208714822a44c51e1a22f
Simon Lentz <simonlentz@SIMON.local>
2022-11-17 15:32:25 +0100

    Remove birthday and modified field

--- lib/ingredients_plug.ex
+++ lib/ingredients_plug.ex
@@ -6,12 +6,10 @@ defmodule IngredientsPlug do
   end
 
   def call(%Plug.Conn{request_path: "/__status"} = conn, _options) do
-    data =
-      %{
-        language: %{name: "elixir", version: System.version()}
-      }
-      |> append_birthday()
-      |> append_revision_and_modified()
+    data = %{
+      language: %{name: "elixir", version: System.version()},
+      vcs: %{revision: revision()}
+    }
 
     conn
     |> put_resp_header("content-type", "application/json")
@@ -23,30 +21,6 @@ defmodule IngredientsPlug do
   end
 
   def call(conn, _options), do: conn
-
-  defp append_birthday(map) do
-    {:ok, birthdatetime} =
-      System.build_info().date |> Timex.parse("{YYYY}-{0M}-{D}T{h24}:{m}:{s}Z")
-
-    {:ok, birthday} = birthdatetime |> Timex.format("{YYYY}-{0M}-{D}")
-
-    Map.put(map, :service, %{birthday: birthday})
-  end
-
-  defp append_revision_and_modified(map) do
-    # {hash, _} = System.cmd("git", ["rev-parse", "HEAD"])
-    # revision = String.trim(hash)
-    # {datetime, _} = System.cmd("git", ["log", "-1", "--format=%cd"])
-
-    # {:ok, parsed_datetime} =
-    # datetime |> String.trim() |> Timex.parse("%a %b %d %T %Y %z", :strftime)
-
-    # {:ok, date} = parsed_datetime |> Timex.format("{YYYY}-{0M}-{D}")
-    # , time: date})
-    Map.put(map, :vcs, %{revision: revision(), time: time()})
-  end
-
-  defp time(), do: System.get_env("UPDATED_AT")
 
   defp revision(), do: System.get_env("COMMIT_SHA")
 end
